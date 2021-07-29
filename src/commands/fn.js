c0e95d18e32bbbca14d95b408956376760742aad
Thomas Schoffelen <thomas@schof.co>
2021-07-29 21:01:46 +0100

    Fix schedule expression formatting.

--- src/commands/fn.js
+++ src/commands/fn.js
@@ -71,7 +71,7 @@ const fn = async (functionName) => {
               },
             ]
           : schedule
-          ? [schedule]
+          ? [{ schedule }]
           : undefined,
     },
   };
